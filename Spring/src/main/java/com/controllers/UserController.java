5525d3ad52f64e0139e6a873987fb2fb6a5eccbd
shimulcse22 <shimul.cse22@gmail.com>
2019-03-21 16:26:54 +0600

    Adding Files

---
+++ Spring/src/main/java/com/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="users")
+public class UserController {
+	
+	@RequestMapping(value="/page",method = RequestMethod.GET)
+	public ModelAndView getPage() {
+		ModelAndView view = new ModelAndView("hello");
+		return view;
+	}
+}
